525a0ece2d2d70fcc7cc81cfde9e47a6f6d5543a
Neo <neo.zulkarnain@gmail.com>
2024-12-25 10:16:22 +0700

    {Neo - Update}

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,48 +5,49 @@ namespace App\Http\Controllers;
 use App\Models\Buku;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+
 class AdminController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $bukuu = Buku::all();
         return view('admin.index', compact('bukuu'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('admin.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $messages = [
-            'required' => ':Attribute harus diisi.',
-            'penerbit' => ':Attribute harus diisi.',
+            'ISBN' => [
+                'numeric' => 'The ISBN must be a number.',
+                'min' => 'The ISBN must be at least 10 digits.',
+                'unique' => 'This ISBN is already taken.',
+            ],
+            'tahun_terbit' => [
+                'max' => 'The Year input is Invalid. ex: 2024.',
+            ],
+            'stok' => [
+                'numeric' => 'The Input must be a number.',
+                'min' => 'The Stock should be more than 1. ex: 21',
+            ],
         ];
 
         $validator = Validator::make($request->all(), [
             'judul_buku' => 'required|string|max:255',
-            'penerbit' => 'required|string',
-            'penulis' => 'required|string',
-            'ISBN' => 'required|numeric|unique:buku,ISBN',
-            'tahun_terbit' => 'required|numeric',
-            'stok' => 'required|numeric',
+            'penerbit' => 'required|string|max:255',
+            'penulis' => 'required|string|max:255',
+            'ISBN' => 'required|numeric|min:1111111111|unique:buku,ISBN', //ISBN Min.10, Max.13
+            'tahun_terbit' => 'required|numeric|max:9999',
+            'stok' => 'required|numeric|min:1',
         ], $messages);
 
         if ($validator->fails()) {
             return redirect()->back()->withErrors($validator)->withInput();
         }
-
         $buku = new Buku;
         $buku->judul_buku = $request->judul_buku;
         $buku->penerbit = $request->penerbit;
@@ -59,39 +60,26 @@ class AdminController extends Controller
         return redirect()->route('dashboard-admin.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         $buku = Buku::findOrFail($id);
         return view('admin.edit', compact('buku'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         $buku = Buku::find($id);
         $buku->delete();
         return redirect()->route('dashboard-admin.index');
-
     }
 }
